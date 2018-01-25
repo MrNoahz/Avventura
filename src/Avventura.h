484296d3d47c7ed1ee5cd1b2ed943d30a935b175
Noah Bresler <noahbresler2002@gmail.com>
2018-01-25 13:44:31 -0600

    TFT Library

--- src/Avventura.h
+++ src/Avventura.h
@@ -1,12 +1,13 @@
 // #include <Esplora.h>
 // #include <TFT.h>
-// #include <SPI.h>
 // #include "core/TFT.h"
+#include <SPI.h>
+#include "gfx/TFT_ST7735/TFT_ST7735.h"
 
 #ifndef Avventura_h
 #define Avventura_h
 
-#include <Esplora.h>
+// #include <Esplora.h>
 class Avventura {
 public:
 	Avventura();
